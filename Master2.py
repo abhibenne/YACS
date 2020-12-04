6f893e9f58911922bbb5643dff87405e9e340a2b
hemanth-alva <51407911+hemanth-alva@users.noreply.github.com>
2020-12-04 21:20:41 +0530

    Update Master2.py

--- Master2.py
+++ Master2.py
@@ -147,46 +147,6 @@ def roundRobin(chosenTask):
 	#print('Task with task id ',chosenTask['task_id'],' is being scheduled on worker with id',currentConfiguration['workers'][workerNumber]['worker_id'])
 
 
-def roundRobin2(chosenTask):
-	numberOfWorkers = len(currentConfiguration['workers'])
-	workerNumber=0
-	workerId = 0
-	configurationLock.acquire()
-	copyConfig = copy.deepcopy(currentConfiguration['workers'])
-	copyConfig.sort(key=lambda x:x['worker_id'])
-	workerId = copyConfig[workerNumber]['worker_id']
-	while copyConfig[workerNumber]['slots'] == 0:
-		configurationLock.release()
-		time.sleep(1)
-		workerNumber = (workerNumber+1)%numberOfWorkers
-		configurationLock.acquire()
-		copyConfig = copy.deepcopy(currentConfiguration['workers'])
-		copyConfig.sort(key=lambda x:x['worker_id'])
-		workerId = copyConfig[workerNumber]['worker_id']
-	configurationLock.release()
-	#currentConfiguration['workers'][workerNumber]['slots']-=1
-	#print(currentConfiguration)
-	#print("\n")
-	indexNumber=-1
-	#minLoadingIndex=currentConfiguration['workers'].find(currentConfiguration['workers']['worker_id']=minLoadingIndex)
-	for i in range(0,len(currentConfiguration['workers'])):
-		if(currentConfiguration['workers'][i]['worker_id']==workerId):
-			indexNumber=i
-			break
-
-	configurationLock.acquire()
-	currentConfiguration['workers'][indexNumber]['slots']-=1
-	#print(currentConfiguration)
-	#print("\n")
-	#print(currentConfiguration['workers'][minLoadingIndex]['slots'],' is slots')
-	configurationLock.release()
-
-	#print(chosenTask)
-	sendToWorker(chosenTask,indexNumber)
-	#print('Task with task id ',chosenTask['task_id'],' is being scheduled on worker with id',currentConfiguration['workers'][workerNumber]['worker_id'])
-
-
-
 def leastLoaded(chosenTask):
 	numberOfWorkers = len(currentConfiguration['workers'])
 	workerNumber=0
@@ -209,7 +169,7 @@ def leastLoaded(chosenTask):
 		#if workerNumber==numberOfWorkers:
 		#	break;
 		#configurationLock.acquire()
-	#print(currentConfiguration)
+	print(currentConfiguration)
 	#print(minLoadingIndex)
 	configurationLock.release()
 	
@@ -351,9 +311,7 @@ def recieveUpdates():
 				finishReducerLock.release()
 		#print(taskLogs,' that was TASK_LOG')
 		print("\n----*******----")
-		#configurationLock.acquire()
 		print(currentConfiguration,' is current configuration')
-		#configurationLock.release()
 		print("----*******----\n")
 
 thread1 = threading.Thread(target = acceptRequest)
